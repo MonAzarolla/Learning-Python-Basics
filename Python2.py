336c04d8e4122b3d70dea93e97c2b96057b7d227
AzarollaDev <128243732+MonAzarolla@users.noreply.github.com>
2023-03-19 17:45:45 -0400

    Add files via upload

---
+++ Python2.py
@@ -0,0 +1,44 @@
+# Now it will print the text three times, and that is so neat, and very helpful.
+
+
+# 1) How can we put an if statement within a function
+
+
+def college_age_calculator(age,name):
+    if age < 18:
+        print("Enjoy the time in highschool because being an adult is stressful", name, "is only", age)
+
+
+elif age  ==18:
+
+
+print ("Enjoy highschool",name):
+else:
+
+
+                print("Being young means you get to experience life while being an adult means you have a lot of responsibilities.")
+               
+                college_age_calculator(18,"Maxwell")
+
+
+# 2 ) Now we will start to create another function
+
+
+# We will see what our age will be in 10 years.
+
+
+def add_fifteen_to_age(age):
+
+
+new_age = age + 10
+
+
+return new_age
+
+
+How_Old_Will_I_Be = add_fifteen_to_age(20)
+
+
+    print (How_Old_Will_I_Be)
+
+    
